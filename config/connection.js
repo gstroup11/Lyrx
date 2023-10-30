35e0aa5c38ddb4b5092549de28dfb8a423afb1fb
gstroup11 <gstroup11@gmail.com>
2023-10-30 19:42:26 -0400

    Pushes up base controllers for the ladning page and some minor bug tweaks throughout.

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
-    process.env.DB_USER,
+    process.env.DB_USERNAME,
     process.env.DB_PASSWORD,
     {
       host: 'localhost',
