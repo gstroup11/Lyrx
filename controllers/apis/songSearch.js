345003b383840631b4467bd2ac05fe174af03821
gstroup11 <gstroup11@gmail.com>
2023-11-03 15:28:21 -0400

    Added in all routes, put nav bar on search page

--- controllers/apis/songSearch.js
+++ controllers/apis/songSearch.js
@@ -32,5 +32,41 @@ router.get("/result", async (req, res) => {
   }
 });
 
+router.get('/contact', async (req, res) => {
+  try {
+    res.render('contact');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/home', async (req, res) => {
+  try {
+    res.render('home');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// renders login page
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// renders signup page
+router.get('/signup', async (req, res) => {
+  try {
+    res.render('signup');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 module.exports = router;
 
