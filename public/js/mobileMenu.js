e040e7bf507b184473f72f22f08f25b46bae1b47
auom.eisiad <daisiemouaa@gmail.com>
2023-11-06 17:49:24 -0500

    fixing media query for results page

--- public/js/mobileMenu.js
+++ public/js/mobileMenu.js
@@ -1,26 +1,35 @@
-
 const logo = document.querySelector('.logo');
 const menu = document.querySelector('nav');
-// Prevent the menu from closing when clicking inside it
-menu.addEventListener('click', (e) => {
-  e.stopPropagation();
-});
+let menuIsOpen = false; // Maintain a variable to track the menu state
+
 // Close the menu when clicking outside of it
 document.addEventListener('click', () => {
-  menu.style.display = 'none';
+  if (menuIsOpen) {
+    menu.style.display = 'none';
+    menuIsOpen = false;
+  }
 });
+
 // Toggle the menu when clicking on the logo
 logo.addEventListener('click', (e) => {
   e.stopPropagation(); // Prevent the document click event from immediately closing the menu
-  menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
-});
-function handleMenuVisibility() {
-  if (window.innerWidth < 575) {
+  if (menuIsOpen) {
     menu.style.display = 'none';
   } else {
     menu.style.display = 'flex';
   }
+  menuIsOpen = !menuIsOpen;
+});
+
+function handleMenuVisibility() {
+  if (window.innerWidth < 575) {
+    menu.style.display = 'none';
+    menuIsOpen = false; // Ensure menu is closed if the window is too narrow
+  } else {
+    menu.style.display = 'flex';
+  }
 }
+
 // Initial check and listen for window resize
 handleMenuVisibility();
 window.addEventListener('resize', handleMenuVisibility);
