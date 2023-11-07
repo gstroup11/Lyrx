c5a89cda92897f9c1a92cf7a3471166b10d04c6a
auom.eisiad <daisiemouaa@gmail.com>
2023-11-06 20:04:30 -0500

    fixed responsiveness for all handlebars

--- public/js/mobileMenu.js
+++ public/js/mobileMenu.js
@@ -1,35 +1,52 @@
-const logo = document.querySelectorAll('.logo');
-const menu = document.querySelectorAll('nav');
-let menuIsOpen = false; // Maintain a variable to track the menu state
+const logoElements = document.querySelectorAll('.logo');
+const menuElements = document.querySelectorAll('nav');
 
-// Close the menu when clicking outside of it
-document.addEventListener('click', () => {
-  if (menuIsOpen) {
-    menu.style.display = 'none';
-    menuIsOpen = false;
-  }
-});
-
-// Toggle the menu when clicking on the logo
-logo.addEventListener('click', (e) => {
-  e.stopPropagation(); // Prevent the document click event from immediately closing the menu
-  if (menuIsOpen) {
-    menu.style.display = 'none';
-  } else {
-    menu.style.display = 'flex';
-  }
-  menuIsOpen = !menuIsOpen;
-});
-
-function handleMenuVisibility() {
+function handleMenuVisibility(menu, menuIsOpen) {
   if (window.innerWidth < 575) {
     menu.style.display = 'none';
-    menuIsOpen = false; // Ensure menu is closed if the window is too narrow
+    menuIsOpen = false; 
   } else {
     menu.style.display = 'flex';
   }
+  return menuIsOpen;
 }
 
-// Initial check and listen for window resize
-handleMenuVisibility();
-window.addEventListener('resize', handleMenuVisibility);
+const menuStates = new Map();
+
+logoElements.forEach((logo, index) => {
+  menuStates.set(logo, false);
+
+  logo.addEventListener('click', (e) => {
+    e.stopPropagation(); 
+    const menuIsOpen = menuStates.get(logo);
+    const menu = menuElements[index];
+    if (menuIsOpen) {
+      menu.style.display = 'none';
+    } else {
+      menu.style.display = 'flex';
+    }
+    menuStates.set(logo, !menuIsOpen);
+  });
+});
+
+document.addEventListener('click', () => {
+  logoElements.forEach((logo, index) => {
+    const menuIsOpen = menuStates.get(logo);
+    if (menuIsOpen) {
+      menuElements[index].style.display = 'none';
+      menuStates.set(logo, false);
+    }
+  });
+});
+
+function handleAllMenuVisibility() {
+  logoElements.forEach((logo, index) => {
+    const menu = menuElements[index];
+    let menuIsOpen = menuStates.get(logo);
+    menuIsOpen = handleMenuVisibility(menu, menuIsOpen);
+    menuStates.set(logo, menuIsOpen);
+  });
+}
+
+handleAllMenuVisibility();
+window.addEventListener('resize', handleAllMenuVisibility);
